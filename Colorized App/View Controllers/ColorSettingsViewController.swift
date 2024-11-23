d4033409d8d8c51ab6ad3e31d36aacee09d8c7a3
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-11-23 17:46:55 +0300

    added Hiding The Keyboard

--- Colorized App/View Controllers/ColorSettingsViewController.swift
+++ Colorized App/View Controllers/ColorSettingsViewController.swift
@@ -20,6 +20,7 @@ final class ColorSettingsViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    
     var deligate: ColorSettingsViewControllerDeligate!
     var viewColor: UIColor!
     
@@ -37,8 +38,7 @@ final class ColorSettingsViewController: UIViewController {
         // MARK: - Setting The Initial Values
         setValue(for: redSlider, greenSlider, blueSlider)
         setValue(for: redLabel, greenLabel, blueLabel)
-        setValue(for:)
-        
+        setValue(for: redTextField, greenTextField, blueTextField)
         
         setColor()
         
@@ -71,32 +71,44 @@ final class ColorSettingsViewController: UIViewController {
             blue: CGFloat(blueSlider.value),
             alpha: 1)
     }
+
     private func setValue(for labels: UILabel...) {
-        labels.forEach { label in switch label {
-        case redLabel: label.text = string(from: redSlider)
-        case greenLabel: label.text = string(from: greenSlider)
-        default: label.text = string(from: blueSlider)
-        }
+        for label in labels {
+            switch label {
+            case redLabel: label.text = string(from: redSlider)
+            case greenLabel: label.text = string(from: greenSlider)
+            default: label.text = string(from: blueSlider)
+            }
         }
     }
     
     private func setValue(for colorSliders: UISlider...) {
         var ciColor = CIColor(color: viewColor)
-        colorSliders.forEach {slider in switch slider {
-        case redSlider: redSlider.value = Float(ciColor.red)
-        case greenSlider: greenSlider.value = Float(ciColor.green)
-        default: blueSlider: blueSlider.value = Float(ciColor.blue)
+        for slider in colorSliders {
+            switch slider {
+            case redSlider: redSlider.value = Float(ciColor.red)
+            case greenSlider: greenSlider.value = Float(ciColor.green)
+            default: blueSlider: blueSlider.value = Float(ciColor.blue)
+            }
         }
-        }
-    }
-    
-    private func showAlert(withTitle title: String, andMessage message: String, textField: UITextField? = nil) {
-        
     }
     
     private func string(from slider: UISlider) -> String {
         "\(round(slider.value * 100) / 100)"
     }
+    
+    // MARK: - Incorrect Input
+    private func showAlert(withTitle title: String, andMessage message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let normalAction = UIActiontitle: "OK", style: .default) {_ in
+            textField?.text = "0.50"
+            textField?.becomeFirstResponder()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    
     
     @IBAction func doneButtonPressedAction() {
         delegate.setColor(colorView.backgroundColor ?? .white)
@@ -104,7 +116,45 @@ final class ColorSettingsViewController: UIViewController {
     }
 }
 
+// MARK: - UITextFieldDelegate
+extension ColorSettingsViewController: UITextFieldDelegate {
+    
+// MARK: - Hiding The Keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    // MARK: - Toolbar With Done Button Except Red
+    func textFieldDidBeginEndEditing(_ textField: UITextField) {
+        guard textField != redTextField else { return }
+        
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.sizeToFit()
+        textField.inputAccessoryView = keyboardToolbar
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: textField, action: #selector(resignFirstResponder))
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        keyboardToolbar.items = [doneButton, flexBarButton]
+    }
+    
+    // MARK: - Checks When Editing Is Completed
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else {
+            showAlert(withTitle: "Wrong format, buddy!", andMessage: "Enter correct value!", textField: textField)
+            return
+        }
+        
+        switch textField {
+        case redTextField: redSlider.setValue(currentValue, animated: true)
+            setValue(for: redLabel)
+        case greeTextField: greenSlider.setValue(currentValue, animated: true)
+            setValue(for: greenLabel)
+        default: blueSlider.setValue(currentValue, animated: true)
+            setValue(for: blueLabel)
+        }
+        setColor()
+    }
+    
 
-
-
-
+}
